9f185dd0942508a674a23c140ef25c07c169c566
Greensi7 <550567@mail.muni.cz>
2026-06-09 11:36:11 +0200

    Add TLS use_srtp parser fuzzer

---
+++ fuzz/tls_use_srtp_parser.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2026 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#include <openssl/crypto.h>
+#include <openssl/err.h>
+#include <openssl/ssl.h>
+#include <openssl/srtp.h>
+#include "internal/packet.h"
+#include "internal/ssl_unwrap.h"
+#include "../ssl/ssl_local.h"
+#include "../ssl/statem/statem_local.h"
+#include "fuzzer.h"
+
+#define MAX_FUZZ_USE_SRTP_INPUT_LEN 4096
+
+int FuzzerInitialize(int *argc, char ***argv)
+{
+    OPENSSL_init_crypto(OPENSSL_INIT_LOAD_CRYPTO_STRINGS, NULL);
+    OPENSSL_init_ssl(OPENSSL_INIT_LOAD_SSL_STRINGS, NULL);
+    ERR_clear_error();
+    CRYPTO_free_ex_index(0, -1);
+    return 1;
+}
+
+int FuzzerTestOneInput(const uint8_t *buf, size_t len)
+{
+    SSL_CTX *ctx = NULL;
+    SSL *ssl = NULL;
+    SSL_CONNECTION *s = NULL;
+    PACKET pkt;
+
+    if (len == 0 || len > MAX_FUZZ_USE_SRTP_INPUT_LEN)
+        return 0;
+
+    if (!PACKET_buf_init(&pkt, buf, len))
+        return 0;
+
+    ctx = SSL_CTX_new(TLS_server_method());
+    if (ctx == NULL)
+        goto end;
+
+    if (SSL_CTX_set_tlsext_use_srtp(ctx,
+            "SRTP_AES128_CM_SHA1_80:SRTP_AES128_CM_SHA1_32:"
+            "SRTP_AEAD_AES_128_GCM:SRTP_AEAD_AES_256_GCM") != 0)
+        goto end;
+
+    ssl = SSL_new(ctx);
+    if (ssl == NULL)
+        goto end;
+
+    SSL_set_accept_state(ssl);
+    s = SSL_CONNECTION_FROM_SSL(ssl);
+    if (s == NULL)
+        goto end;
+
+    tls_parse_ctos_use_srtp(s, &pkt, 0, NULL, 0);
+
+end:
+    SSL_free(ssl);
+    SSL_CTX_free(ctx);
+    ERR_clear_error();
+    return 0;
+}
+
+void FuzzerCleanup(void)
+{
+}
